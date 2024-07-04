8dccc52b08c08623ed69b8cfbe237aa4fd0b2e3e
Paolo Marrone <paolo.marrone@orastron.com>
2024-07-04 10:09:38 +0200

    indentation/alignment

--- templates/web/src/data.h
+++ templates/web/src/data.h
@@ -18,17 +18,17 @@
  * File author: Stefano D'Angelo
  */
 
-#define DATA_PRODUCT_AUDIO_INPUT_CHANNELS_N	{{=it.product.buses.filter(x => x.type == "audio" && x.direction == "input").reduce((s, x) => s += x.channels == "mono" ? 1 : 2, 0)}}
-#define DATA_PRODUCT_AUDIO_OUTPUT_CHANNELS_N	{{=it.product.buses.filter(x => x.type == "audio" && x.direction == "output").reduce((s, x) => s += x.channels == "mono" ? 1 : 2, 0)}}
-#define DATA_PRODUCT_MIDI_INPUTS_N		{{=it.product.buses.filter(x => x.type == "midi" && x.direction == "input").length}}
-#define DATA_PRODUCT_MIDI_OUTPUTS_N		{{=it.product.buses.filter(x => x.type == "midi" && x.direction == "output").length}}
-#define DATA_PRODUCT_PARAMETERS_N		{{=it.product.parameters.length}}
-#define DATA_PRODUCT_PARAMETERS_OUTPUT_N	{{=it.product.parameters.filter(x => x.direction == "output").length}}
+#define DATA_PRODUCT_AUDIO_INPUT_CHANNELS_N  {{=it.product.buses.filter(x => x.type == "audio" && x.direction == "input").reduce((s, x) => s += x.channels == "mono" ? 1 : 2, 0)}}
+#define DATA_PRODUCT_AUDIO_OUTPUT_CHANNELS_N {{=it.product.buses.filter(x => x.type == "audio" && x.direction == "output").reduce((s, x) => s += x.channels == "mono" ? 1 : 2, 0)}}
+#define DATA_PRODUCT_MIDI_INPUTS_N           {{=it.product.buses.filter(x => x.type == "midi" && x.direction == "input").length}}
+#define DATA_PRODUCT_MIDI_OUTPUTS_N          {{=it.product.buses.filter(x => x.type == "midi" && x.direction == "output").length}}
+#define DATA_PRODUCT_PARAMETERS_N            {{=it.product.parameters.length}}
+#define DATA_PRODUCT_PARAMETERS_OUTPUT_N     {{=it.product.parameters.filter(x => x.direction == "output").length}}
 
 #if DATA_PRODUCT_PARAMETERS_N > 0
 static struct {
-	char	out;
-	float	def;
+	char  out;
+	float def;
 } param_data[DATA_PRODUCT_PARAMETERS_N] = {
 {{~it.product.parameters :p}}
 	{
