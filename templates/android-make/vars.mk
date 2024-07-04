4d0ab165b3d89a3e4496b86e7fd7853613a9dd68
Paolo Marrone <paolo.marrone@orastron.com>
2024-07-04 11:21:41 +0200

    proper spacing alignment

--- templates/android-make/vars.mk
+++ templates/android-make/vars.mk
@@ -18,38 +18,38 @@
 # File author: Stefano D'Angelo
 #
 
-BUNDLE_NAME := {{=it.product.bundleName}}
-JAVA_PACKAGE_NAME := {{=it.android.javaPackageName}}
+BUNDLE_NAME                       := {{=it.product.bundleName}}
+JAVA_PACKAGE_NAME                 := {{=it.android.javaPackageName}}
 
-CFLAGS_EXTRA := {{=it.make?.cflags ?? ""}} {{=it.android_make?.cflags ?? ""}}
-CXXFLAGS_EXTRA := {{=it.make?.cxxflags ?? ""}} {{=it.android_make?.cxxflags ?? ""}}
-JFLAGS_EXTRA := {{=it.make?.jflags ?? ""}} {{=it.android_make?.jflags ?? ""}}
-LDFLAGS_EXTRA := {{=it.make?.ldflags ?? ""}} {{=it.android_make?.ldflags ?? ""}}
+CFLAGS_EXTRA                      := {{=it.make?.cflags   ?? ""}} {{=it.android_make?.cflags   ?? ""}}
+CXXFLAGS_EXTRA                    := {{=it.make?.cxxflags ?? ""}} {{=it.android_make?.cxxflags ?? ""}}
+JFLAGS_EXTRA                      := {{=it.make?.jflags   ?? ""}} {{=it.android_make?.jflags   ?? ""}}
+LDFLAGS_EXTRA                     := {{=it.make?.ldflags  ?? ""}} {{=it.android_make?.ldflags  ?? ""}}
 
-C_SRCS_EXTRA := {{=it.make?.cSrcs ?? ""}} {{=it.android_make?.cSrcs ?? ""}}
-CXX_SRCS_EXTRA := {{=it.make?.cxxSrcs ?? ""}} {{=it.android_make?.cxxSrcs ?? ""}}
+C_SRCS_EXTRA                      := {{=it.make?.cSrcs   ?? ""}} {{=it.android_make?.cSrcs   ?? ""}}
+CXX_SRCS_EXTRA                    := {{=it.make?.cxxSrcs ?? ""}} {{=it.android_make?.cxxSrcs ?? ""}}
 
-COMMON_DIR := {{=it.android_make?.commonDir ?? (it.make?.commonDir ?? "")}}
-DATA_DIR := {{=it.android_make?.dataDir ?? (it.make?.dataDir ?? "")}}
-PLUGIN_DIR := {{=it.android_make?.pluginDir ?? (it.make?.pluginDir ?? "")}}
+COMMON_DIR                        := {{=it.android_make?.commonDir ?? (it.make?.commonDir ?? "")}}
+DATA_DIR                          := {{=it.android_make?.dataDir   ?? (it.make?.dataDir   ?? "")}}
+PLUGIN_DIR                        := {{=it.android_make?.pluginDir ?? (it.make?.pluginDir ?? "")}}
 
-KEY_STORE := {{=it.android_make.keyStore}}
-KEY_ALIAS := {{=it.android_make.keyAlias}}
-STORE_PASS := {{=it.android_make.storePass}}
-KEY_PASS := {{=it.android_make.keyPass}}
+KEY_STORE                         := {{=it.android_make.keyStore}}
+KEY_ALIAS                         := {{=it.android_make.keyAlias}}
+STORE_PASS                        := {{=it.android_make.storePass}}
+KEY_PASS                          := {{=it.android_make.keyPass}}
 
-ANDROID_SDK_DIR := {{=it.android_make.sdkDir}}
-ANDROID_NDK_DIR := ${ANDROID_SDK_DIR}/ndk/{{=it.android_make.ndkVersion}}
-BUILD_TOOLS_DIR := ${ANDROID_SDK_DIR}/build-tools/{{=it.android_make.buildToolsVersion}}
-ANDROIDX_DIR := {{=it.android_make.androidxDir}}
-KOTLIN_DIR := {{=it.android_make.kotlinDir}}
+ANDROID_SDK_DIR                   := {{=it.android_make.sdkDir}}
+ANDROID_NDK_DIR                   := ${ANDROID_SDK_DIR}/ndk/{{=it.android_make.ndkVersion}}
+BUILD_TOOLS_DIR                   := ${ANDROID_SDK_DIR}/build-tools/{{=it.android_make.buildToolsVersion}}
+ANDROIDX_DIR                      := {{=it.android_make.androidxDir}}
+KOTLIN_DIR                        := {{=it.android_make.kotlinDir}}
 
-ANDROID_JAR_FILE := ${ANDROID_SDK_DIR}/platforms/android-{{=it.android_make.androidVersion}}/android.jar
-ANDROIDX_CORE_FILE := ${ANDROIDX_DIR}/core-{{=it.android_make.androidxCoreVersion}}.jar
-ANDROIDX_LIFECYCLE_COMMON_FILE := ${ANDROIDX_DIR}/lifecycle-common-{{=it.android_make.androidxLifecycleCommonVersion}}.jar
+ANDROID_JAR_FILE                  := ${ANDROID_SDK_DIR}/platforms/android-{{=it.android_make.androidVersion}}/android.jar
+ANDROIDX_CORE_FILE                := ${ANDROIDX_DIR}/core-{{=it.android_make.androidxCoreVersion}}.jar
+ANDROIDX_LIFECYCLE_COMMON_FILE    := ${ANDROIDX_DIR}/lifecycle-common-{{=it.android_make.androidxLifecycleCommonVersion}}.jar
 ANDROIDX_VERSIONEDPARCELABLE_FILE := ${ANDROIDX_DIR}/versionedparcelable-{{=it.android_make.androidxVersionedparcelableVersion}}.jar
-KOTLIN_STDLIB_FILE := ${KOTLIN_DIR}/kotlin-stdlib-{{=it.android_make.kotlinStdlibVersion}}.jar
-KOTLINX_COROUTINES_CORE_FILE := ${KOTLIN_DIR}/kotlinx-coroutines-core-{{=it.android_make.kotlinxCoroutinesCoreVersion}}.jar
-KOTLINX_COROUTINES_CORE_JVM_FILE := ${KOTLIN_DIR}/kotlinx-coroutines-core-jvm-{{=it.android_make.kotlinxCoroutinesCoreJVMVersion}}.jar
+KOTLIN_STDLIB_FILE                := ${KOTLIN_DIR}/kotlin-stdlib-{{=it.android_make.kotlinStdlibVersion}}.jar
+KOTLINX_COROUTINES_CORE_FILE      := ${KOTLIN_DIR}/kotlinx-coroutines-core-{{=it.android_make.kotlinxCoroutinesCoreVersion}}.jar
+KOTLINX_COROUTINES_CORE_JVM_FILE  := ${KOTLIN_DIR}/kotlinx-coroutines-core-jvm-{{=it.android_make.kotlinxCoroutinesCoreJVMVersion}}.jar
 
-HAS_MIDI_IN := {{=it.product.buses.filter(x => x.type == "midi" && x.direction == "input").length > 0 ? "yes" : "no"}}
+HAS_MIDI_IN                       := {{=it.product.buses.filter(x => x.type == "midi" && x.direction == "input").length > 0 ? "yes" : "no"}}
