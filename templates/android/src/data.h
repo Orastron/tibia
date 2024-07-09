c39b5ed9930588a17dd4eb5eb3e2046c13aba083
Paolo Marrone <lallero.31@gmail.com>
2024-07-09 12:21:11 +0200

    cpumeter android

--- templates/android/src/data.h
+++ templates/android/src/data.h
@@ -18,37 +18,37 @@
  * File author: Stefano D'Angelo
  */
 
-#define NUM_AUDIO_BUSES_IN		{{=it.product.buses.filter(x => x.type == "audio" && x.direction == "input").length}}
-#define NUM_AUDIO_BUSES_OUT		{{=it.product.buses.filter(x => x.type == "audio" && x.direction == "output").length}}
+#define NUM_AUDIO_BUSES_IN       {{=it.product.buses.filter(x => x.type == "audio" && x.direction == "input").length}}
+#define NUM_AUDIO_BUSES_OUT      {{=it.product.buses.filter(x => x.type == "audio" && x.direction == "output").length}}
 
-#define AUDIO_BUS_IN			{{=it.product.buses.findIndex(x => x.type == "audio" && x.direction == "input" && !x.cv && !x.sidechain && !x.optional)}}
-#define AUDIO_BUS_OUT			{{=it.product.buses.findIndex(x => x.type == "audio" && x.direction == "output" && !x.cv && !x.sidechain && !x.optional)}}
+#define AUDIO_BUS_IN             {{=it.product.buses.findIndex(x => x.type == "audio" && x.direction == "input"  && !x.cv && !x.sidechain && !x.optional)}}
+#define AUDIO_BUS_OUT            {{=it.product.buses.findIndex(x => x.type == "audio" && x.direction == "output" && !x.cv && !x.sidechain && !x.optional)}}
 
-#define NUM_CHANNELS_IN			{{=it.product.buses.filter(x => x.type == "audio" && x.direction == "input" && !x.cv && !x.sidechain && !x.optional).length > 0 ? (it.product.buses.filter(x => x.type == "audio" && x.direction == "input" && !x.cv && !x.sidechain && !x.optional)[0].channels == "mono" ? 1 : 2) : 0}}
-#define NUM_CHANNELS_OUT		{{=it.product.buses.filter(x => x.type == "audio" && x.direction == "output" && !x.cv && !x.sidechain && !x.optional).length > 0 ? (it.product.buses.filter(x => x.type == "audio" && x.direction == "output" && !x.cv && !x.sidechain && !x.optional)[0].channels == "mono" ? 1 : 2) : 0}}
-#define NUM_NON_OPT_CHANNELS_IN		{{=it.product.buses.filter(x => x.type == "audio" && x.direction == "input" && !x.optional).reduce((a, x) => a + (x.channels == "mono" ? 1 : 2), 0)}}
-#define NUM_NON_OPT_CHANNELS_OUT	{{=it.product.buses.filter(x => x.type == "audio" && x.direction == "output" && !x.optional).reduce((a, x) => a + (x.channels == "mono" ? 1 : 2), 0)}}
-#define NUM_ALL_CHANNELS_IN		{{=it.product.buses.filter(x => x.type == "audio" && x.direction == "input").reduce((a, x) => a + (x.channels == "mono" ? 1 : 2), 0)}}
-#define NUM_ALL_CHANNELS_OUT		{{=it.product.buses.filter(x => x.type == "audio" && x.direction == "output").reduce((a, x) => a + (x.channels == "mono" ? 1 : 2), 0)}}
+#define NUM_CHANNELS_IN          {{=it.product.buses.filter(x => x.type == "audio" && x.direction == "input"  && !x.cv && !x.sidechain && !x.optional).length > 0 ? (it.product.buses.filter(x => x.type == "audio" && x.direction == "input" && !x.cv && !x.sidechain && !x.optional)[0].channels == "mono" ? 1 : 2) : 0}}
+#define NUM_CHANNELS_OUT         {{=it.product.buses.filter(x => x.type == "audio" && x.direction == "output" && !x.cv && !x.sidechain && !x.optional).length > 0 ? (it.product.buses.filter(x => x.type == "audio" && x.direction == "output" && !x.cv && !x.sidechain && !x.optional)[0].channels == "mono" ? 1 : 2) : 0}}
+#define NUM_NON_OPT_CHANNELS_IN  {{=it.product.buses.filter(x => x.type == "audio" && x.direction == "input"  && !x.optional).reduce((a, x) => a + (x.channels == "mono" ? 1 : 2), 0)}}
+#define NUM_NON_OPT_CHANNELS_OUT {{=it.product.buses.filter(x => x.type == "audio" && x.direction == "output" && !x.optional).reduce((a, x) => a + (x.channels == "mono" ? 1 : 2), 0)}}
+#define NUM_ALL_CHANNELS_IN      {{=it.product.buses.filter(x => x.type == "audio" && x.direction == "input") .reduce((a, x) => a + (x.channels == "mono" ? 1 : 2), 0)}}
+#define NUM_ALL_CHANNELS_OUT     {{=it.product.buses.filter(x => x.type == "audio" && x.direction == "output").reduce((a, x) => a + (x.channels == "mono" ? 1 : 2), 0)}}
 
-#define NUM_MIDI_INPUTS			{{=it.product.buses.filter(x => x.type == "midi" && x.direction == "input").length}}
+#define NUM_MIDI_INPUTS          {{=it.product.buses.filter(x => x.type == "midi" && x.direction == "input").length}}
 
-#define MIDI_BUS_IN			{{=it.product.buses.findIndex(x => x.type == "midi" && x.direction == "input")}}
+#define MIDI_BUS_IN              {{=it.product.buses.findIndex(x => x.type == "midi" && x.direction == "input")}}
 
 #if NUM_AUDIO_BUSES_IN + NUM_AUDIO_BUSES_OUT > 0
 static struct {
-	size_t	index;
-	char	out;
-	char	optional;
-	char	channels;
+	size_t index;
+	char   out;
+	char   optional;
+	char   channels;
 } audio_bus_data[NUM_AUDIO_BUSES_IN + NUM_AUDIO_BUSES_OUT] = {
 {{~it.product.buses :b:i}}
 {{?b.type == "audio"}}
 	{
-		/* .index	= */ {{=i}},
-		/* .out		= */ {{=b.direction == "output" ? 1 : 0}},
-		/* .optional	= */ {{=b.optional ? 1 : 0}},
-		/* .channels	= */ {{=b.channels == "mono" ? 1 : 2}}
+		/* .index    = */ {{=i}},
+		/* .out      = */ {{=b.direction == "output" ? 1 : 0}},
+		/* .optional = */ {{=b.optional ? 1 : 0}},
+		/* .channels = */ {{=b.channels == "mono" ? 1 : 2}}
 	},
 {{?}}
 {{~}}
@@ -72,14 +72,19 @@ static struct {
 } param_data[PARAMETERS_N] = {
 {{~it.product.parameters :p}}
 	{
-		/* .out		= */ {{=p.direction == "output" ? 1 : 0}},
-		/* .def		= */ {{=p.defaultValue.toExponential()}},
-		/* .min		= */ {{=p.minimum.toExponential()}}f,
-		/* .max		= */ {{=p.maximum.toExponential()}}f,
-		/* .flags	= */ {{?p.isBypass}}PARAM_BYPASS{{??}}0{{?p.toggled}} | PARAM_TOGGLED{{?}}{{?p.integer}} | PARAM_INTEGER{{?}}{{?}}
+		/* .out   = */ {{=p.direction == "output" ? 1 : 0}},
+		/* .def   = */ {{=p.defaultValue.toExponential()}},
+		/* .min   = */ {{=p.minimum.toExponential()}}f,
+		/* .max   = */ {{=p.maximum.toExponential()}}f,
+		/* .flags = */ {{?p.isBypass}}PARAM_BYPASS{{??}}0{{?p.toggled}} | PARAM_TOGGLED{{?}}{{?p.integer}} | PARAM_INTEGER{{?}}{{?}}
 	},
 {{~}}
 };
+
+{{?it.product.parameters.find(x => x.direction == "output" && x.isCpumeter)}}
+# define PARAM_OUT_CPU_INDEX {{=it.product.parameters.indexOf(it.product.parameters.find(x => x.direction == "output" && x.isCpumeter))}}
+{{?}}
+
 #endif
 
 #define JNI_FUNC(x) Java_{{=it.android.javaPackageName.replaceAll("_", "_1").replaceAll(".", "_")}}_MainActivity_##x
--- test/run.sh
+++ test/run.sh
@@ -18,6 +18,7 @@ cp $dir/plugin.h $dir/../out/web/src
 
 $dir/../tibia $dir/product.json,$dir/company.json,$dir/android.json $dir/../templates/android $dir/../out/android
 $dir/../tibia $dir/product.json,$dir/company.json,$dir/android.json,$dir/android-make.json $dir/../templates/android-make $dir/../out/android
+cp $dir/../templates/common/* $dir/../out/android/src/
 cp $dir/keystore.jks $dir/../out/android
 cp $dir/plugin.h $dir/../out/android/src
 
