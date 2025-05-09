8eaf255762ba662d9520a7c9a0c2c94545e27087
Stefano D'Angelo <s@dangelo.audio>
2025-05-09 15:32:10 +0200

    let javascript module only export buses and parameters

--- templates/web/src/module.js
+++ templates/web/src/module.js
@@ -1,7 +1,7 @@
 /*
  * Tibia
  *
- * Copyright (C) 2022-2024 Orastron Srl unipersonale
+ * Copyright (C) 2022-2025 Orastron Srl unipersonale
  *
  * Tibia is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,13 +18,12 @@
  * File author: Stefano D'Angelo
  */
 
-const data = {
-	company: {{=JSON.stringify(it.company, null, 2)}},
-	product: {{=JSON.stringify(it.product, null, 2)}}
-};
+const buses = {{=JSON.stringify(it.product.buses, null, 2)}};
+const parameters = {{=JSON.stringify(it.product.parameters, null, 2)}};
 
 export class Module {
-	static get data() { return data; }
+	static get buses() { return buses; }
+	static get parameters() { return parameters; }
 
 	async init(context, processorJsPath, wasmPath) {
 		var wasmBytes = await fetch(wasmPath)
@@ -45,11 +44,11 @@ export class Module {
 
 export class Node extends AudioWorkletNode {
 	constructor(module) {
-		super(module.context, data.product.bundleName,
+		super(module.context, "{{=it.product.bundleName}}",
 			{
-				numberOfInputs: data.product.buses.filter(b => b.type == "audio" && b.direction == "input").length,
-				numberOfOutputs: data.product.buses.filter(b => b.type == "audio" && b.direction == "output").length,
-				outputChannelCount: data.product.buses.filter(b => b.type == "audio" && b.direction == "output").map(b => b.channels == "mono" ? 1 : 2),
+				numberOfInputs: buses.filter(b => b.type == "audio" && b.direction == "input").length,
+				numberOfOutputs: buses.filter(b => b.type == "audio" && b.direction == "output").length,
+				outputChannelCount: buses.filter(b => b.type == "audio" && b.direction == "output").map(b => b.channels == "mono" ? 1 : 2),
 				processorOptions: { wasmBytes: module.wasmBytes }
 			});
 	}
