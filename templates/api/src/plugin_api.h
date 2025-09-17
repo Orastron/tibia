17b518038e4b4ba849bdab81be8906d716e8025e
Paolo Marrone <lallero.31@gmail.com>
2025-09-17 17:47:09 +0200

    new get_hostinfo in plugin_ui_callbacks

--- templates/api/src/plugin_api.h
+++ templates/api/src/plugin_api.h
@@ -45,6 +45,7 @@ typedef struct {
 	const char *	format;
 	const char * (*get_bindir)(void *handle);
 	const char * (*get_datadir)(void *handle);
+	const char * (*get_hostinfo)(void *handle);
 {{?it.product.parameters.find(x => x.direction == "input")}}
 	void (*set_parameter_begin)(void *handle, size_t index, float value);
 	void (*set_parameter)(void *handle, size_t index, float value);
